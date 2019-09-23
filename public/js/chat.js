8e46fb59cdf4387a4739ee2ec19648e7fdf227d5
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-09-23 17:27:56 -0300

    basic chat functionality with rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,32 +1,61 @@
 const socket = io();
 
-socket.on("message", message => {
-  console.log(message);
+// Elements
+const $messageForm = document.querySelector("form#message-form");
+const $messageFormInput = document.querySelector("input#message-input");
+const $messageFormButton = document.querySelector("button#send-message-button");
+const $sendLocationButton = document.querySelector("#send-location-button");
+const $messages = document.querySelector("div#messages");
+
+// Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationTemplate = document.querySelector("#location-message-template")
+  .innerHTML;
+
+// Options
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
 });
 
-const messageForm = document.querySelector("form#message-form");
-const messageInput = document.querySelector(
-  "form#message-form input#message-input"
-);
+socket.on("message", message => {
+  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format("h:mm a")
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
 
-messageForm.addEventListener("submit", event => {
+socket.on("locationMessage", locationMessage => {
+  console.log(locationMessage);
+  const html = Mustache.render(locationTemplate, {
+    locationUrl: locationMessage.url,
+    createdAt: moment(locationMessage.createdAt).format("h:mm a")
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+$messageForm.addEventListener("submit", event => {
   event.preventDefault();
 
-  const message = messageInput.value;
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  const message = $messageFormInput.value;
 
   socket.emit("sendMessage", message, ackMessage => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
     console.log(ackMessage);
   });
 });
 
-const sendLocationButton = document.querySelector(
-  "button#send-location-button"
-);
-
-sendLocationButton.addEventListener("click", () => {
+$sendLocationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser!");
   }
+
+  $sendLocationButton.setAttribute("disabled", "disabled");
 
   navigator.geolocation.getCurrentPosition(position => {
     socket.emit(
@@ -36,8 +65,11 @@ sendLocationButton.addEventListener("click", () => {
         longitude: position.coords.longitude
       },
       ackMessage => {
+        $sendLocationButton.removeAttribute("disabled");
         console.log(ackMessage);
       }
     );
   });
 });
+
+socket.emit("join", { username, room });
